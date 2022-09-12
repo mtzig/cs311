47c08c2c46f0af052eef2ce20503708a3cc3f9f0
Thomas Zeng <tomzeng200@gmail.com>
2022-09-12 17:00:51 -0500

    adds basic functionality to 050

--- day01/050mainPainting.c
+++ day01/050mainPainting.c
@@ -19,39 +19,50 @@ Compile with:
 double red = 1.0, green = 1.0, blue = 1.0;
 int mouseIsDown = 0;
 
-void handleKeyUp(int key, int shiftIsDown, int controlIsDown,
+void handleKeyDown(int key, int shiftIsDown, int controlIsDown,
         int altOptionIsDown, int superCommandIsDown) {
     printf("handleKeyUp: %d, shift %d, control %d, altOpt %d, supComm %d\n",
         key, shiftIsDown, controlIsDown, altOptionIsDown, superCommandIsDown);
-    if (key == GLFW_KEY_A)
-        printf("handleKeyUp: I see that you pressed the A key. Interesting!\n");
-    if (key == GLFW_KEY_RIGHT)
-        printf("handleKeyUp: You pressed the right-arrow key. Keep up the good work.\n");
+    // R, G, B, C, M, Y, K, and W
+    if (key == GLFW_KEY_R)
+        red = 1.0, green = 0.0, blue = 0.0;
+    else if (key == GLFW_KEY_G)
+        red = 0.0, green = 1.0, blue = 0.0;
+    else if (key == GLFW_KEY_B)
+        red = 0.0, green = 0.0, blue = 1.0;
+    else if (key == GLFW_KEY_C)
+        red = 0.0, green = 1.0, blue = 1.0;
+    else if (key == GLFW_KEY_M)
+        red = 1.0, green = 0.0, blue = 1.0;
+    else if (key == GLFW_KEY_Y)
+        red = 1.0, green = 1.0, blue = 0.0;
+    else if (key == GLFW_KEY_K)
+        red = 0.0, green = 0.0, blue = 0.0;
+    else if (key == GLFW_KEY_W)
+        red = 1.0, green = 1.0, blue = 1.0;
+    else if (key == GLFW_KEY_Q)
+        pixClearRGB(0.0, 0.0, 0.0);
+
+
 }
 
 /* Similarly, the following callbacks handle some mouse interactions. */
 void handleMouseUp(double x, double y, int button, int shiftIsDown, 
         int controlIsDown, int altOptionIsDown, int superCommandIsDown) {
-    printf("handleMouseUp: %d, shift %d, control %d, altOpt %d, supComm %d\n",
-        button, shiftIsDown, controlIsDown, altOptionIsDown, 
-        superCommandIsDown);
+        mouseIsDown = 0;
 }
 
+void handleMouseDown(double x, double y, int button, int shiftIsDown, 
+        int controlIsDown, int altOptionIsDown, int superCommandIsDown) {
+        mouseIsDown = 1;
+}
 void handleMouseMove(double x, double y) {
-    printf("handleMouseMove: x %f, y %f\n", x, y);
+    if (mouseIsDown == 1)
+        pixSetRGB(x,y, red, green, blue);
 }
 
-void handleMouseScroll(double xOffset, double yOffset) {
-    printf("handleMouseScroll: xOffset %f, yOffset %f\n", xOffset, yOffset);
-}
 
-/* This callback is called once per animation frame. As parameters it receives 
-the time for the current frame and the time for the previous frame. Both times 
-are measured in seconds since some distant past time. */ 
-void handleTimeStep(double oldTime, double newTime) {
-    if (floor(newTime) - floor(oldTime) >= 1.0)
-        printf("handleTimeStep: %f frames/s\n", 1.0 / (newTime - oldTime));
-}
+
 
 /* You can also set callbacks for key down, key repeat, and mouse down. See 
 040pixel.h for details. */
@@ -63,18 +74,14 @@ int main(void) {
         return 1;
     /* Register the callbacks (defined above) with the user interface, so that 
     they are called as needed during pixRun (invoked below). */
-    pixSetKeyUpHandler(handleKeyUp);
+    pixSetKeyDownHandler(handleKeyDown);
     pixSetMouseUpHandler(handleMouseUp);
+    pixSetMouseDownHandler(handleMouseDown);
     pixSetMouseMoveHandler(handleMouseMove);
-    pixSetMouseScrollHandler(handleMouseScroll);
-    pixSetTimeStepHandler(handleTimeStep);
+
     /* Clear the window to black. */
     pixClearRGB(0.0, 0.0, 0.0);
-    /* Draw the graph of y = 500 sin (x / 50) in yellow. Notice that pixSetRGB 
-    ignores attempts to set pixels outside the window. */
-    int i;
-    for (i = 0; i < 512; i = i + 1)
-        pixSetRGB(i, 500.0 * sin(i / 50.0), 1.0, 1.0, 0.0);
+
     /* Run the event loop. The callbacks that were registered above are 
     invoked as needed. */
     pixRun();
