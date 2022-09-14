6fcc82f35da790ba420f163d13be10810beaf70a
Thomas Zeng <tomzeng200@gmail.com>
2022-09-14 16:41:07 -0500

    adds basic scafold of rastorization alg

---
+++ 060triangle.c
@@ -0,0 +1,7 @@
+#include "040pixel.h" //don't know if I need this
+
+void triRender(
+        double a0, double a1, double b0, double b1, double c0, double c1, 
+        double r, double g, double b) {
+	
+}
