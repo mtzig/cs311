9160a649cd73746a2d79e77f64ca36c291ae8f0a
Thomas Zeng <tomzeng200@gmail.com>
2022-09-15 13:14:57 -0500

    changes rasterization structure for clarity

--- 060triangle.c
+++ 060triangle.c
@@ -2,6 +2,14 @@
 #include <math.h>
 
 
+/*
+Creates point-slope equation on (i0, i1), (j0, j1)
+Calculates value on x0
+ */
+double pointSlope(double x0, double i0, double i1, double j0, double j1){
+    return i1 + (j1-i1) / (j0 - i0) * (x0 - i0);
+}
+
 void triRenderWithAleft(
         double a0, double a1, double b0, double b1, double c0, double c1, 
         double r, double g, double b) {
@@ -10,8 +18,8 @@ void triRenderWithAleft(
 
         if (a0 != c0){ //render left half
             for (int x0 = (int) ceil(a0); x0 <= floor(c0); x0++){
-                int bottom = (int) ceil(a1 + (b1-a1) / (b0 - a0) * (x0 - a0));
-                int top = (int) floor(a1 + (c1-a1) / (c0 - a0) * (x0 - a0));
+                int bottom = (int) ceil(pointSlope(x0, a0, a1, b0, b1));
+                int top = (int) floor(pointSlope(x0, a0, a1, c0, c1));
 
                 for (int x1 = bottom; x1 <= top; x1++)
                     pixSetRGB(x0, x1, r, g, b);
@@ -21,8 +29,8 @@ void triRenderWithAleft(
 
         if (b0 != c0){ //render right half
             for (int x0 = (int) floor(c0) + 1; x0 <= floor(b0); x0++){
-                int bottom = (int) ceil(a1 + (b1-a1) / (b0 - a0) * (x0 - a0));
-                int top = (int) floor(b1 + (c1-b1) / (c0 - b0) * (x0 - b0));
+                int bottom = (int) ceil(pointSlope(x0, a0, a1, b0, b1));
+                int top = (int) floor(pointSlope(x0, c0, c1, b0, b1));
 
                 for (int x1 = bottom; x1 <= top; x1++)
                     pixSetRGB(x0, x1, r, g, b);
@@ -35,8 +43,8 @@ void triRenderWithAleft(
 
         if (a0 != b0){ //render left half
             for (int x0 = (int) ceil(a0); x0 <= floor(b0); x0++){
-                int bottom = (int) ceil(a1 + (b1-a1) / (b0 - a0) * (x0 - a0));
-                int top = (int) floor(a1 + (c1-a1) / (c0 - a0) * (x0 - a0));
+                int bottom = (int) ceil(pointSlope(x0, a0, a1, b0, b1));
+                int top = (int) floor(pointSlope(x0, a0, a1, c0, c1));
 
                 for (int x1 = bottom; x1 <= top; x1++)
                     pixSetRGB(x0, x1, r, g, b);
@@ -46,8 +54,8 @@ void triRenderWithAleft(
 
         if (b0 != c0){ //render right half
             for (int x0 = (int) floor(b0) + 1; x0 <= floor(c0); x0++){
-                int bottom = (int) ceil(b1 + (c1-b1) / (c0 - b0) * (x0 - b0));
-                int top = (int) floor(a1 + (c1-a1) / (c0 - a0) * (x0 - a0));
+                int bottom = (int) ceil(pointSlope(x0, c0, c1, b0, b1));
+                int top = (int) floor(pointSlope(x0, a0, a1, c0, c1));
 
                 for (int x1 = bottom; x1 <= top; x1++)
                     pixSetRGB(x0, x1, r, g, b);
